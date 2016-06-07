11a8f08872124cab990581f4c9e5d8c5d8853581
carlgeorge <carlgeorge@carls-mbp-4>
2016-06-07 12:48:31 -0400

    commit
    Change-Id: I26b72808c0570fbbc792b03c43f668c5dafe84d5

--- src/main/webapp/_include/js/main.js
+++ src/main/webapp/_include/js/main.js
@@ -80,7 +80,7 @@ BAKMOTOR.slider = function(){
 											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''},
-											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''}  
+											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''}   
 									],
 									
 		// Theme Options			   
