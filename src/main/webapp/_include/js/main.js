348add109efcf187cd709359837a23df84e4da9d
carlgeorge <carlgeorge@carls-mbp-4>
2016-06-07 11:58:35 -0400

    logo

--- src/main/webapp/_include/js/main.js
+++ src/main/webapp/_include/js/main.js
@@ -77,7 +77,7 @@ BAKMOTOR.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : '_include/img/slider-images/image01.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''},
+											{image : 'http://fontmeme.com/freefonts/img.php?f=33452&s=55&c=FFFFFF', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image02.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image03.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''},
 											{image : '_include/img/slider-images/image04.jpg', title : '<div class="slide-content">BAK MotorSports</div>', thumb : '', url : ''}  
